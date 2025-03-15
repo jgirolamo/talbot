8602df63f37265cd008bd3dd2445a8c73c3adee0
Gui Neto <gcabride@gmail.com>
2025-03-15 01:31:39 +0000

    comment out some of the logging function

--- src/logging_config.py
+++ src/logging_config.py
@@ -11,22 +11,21 @@ def setup_logging():
     This function sets up logging with both a file handler and a stream (console) handler.
     It ensures that all logs include the timestamp, log level, logger name, and message.
     """
-    LOG_DIR = "/app/logs"
-    LOG_FILE = os.path.join(LOG_DIR, "bot_debug.log")
+    log_dir = "/app/logs"
+    #log_file = os.path.join(log_dir, "bot_debug.log")
 
     # Ensure the log directory exists
-    os.makedirs(LOG_DIR, exist_ok=True)
+    os.makedirs(log_dir, exist_ok=True)
 
-    def setup_logging():
-        logging.basicConfig(
-            level=logging.INFO,
-            format="%(asctime)s - %(levelname)s - %(message)s",
-            handlers=[
-                logging.FileHandler(LOG_FILE),
-                logging.StreamHandler()
-            ]
-        
-        )
+    #def setup_logging_config():
+    #    logging.basicConfig(
+    #        level=logging.INFO,
+    #        format="%(asctime)s - %(levelname)s - %(message)s",
+    #        handlers=[
+    #            logging.FileHandler(log_file),
+    #            logging.StreamHandler()
+    #        ]
+    #    )
 
     # Check if the root logger already has handlers to avoid adding duplicate ones
     if not logging.getLogger().hasHandlers():
@@ -34,7 +33,7 @@ def setup_logging():
             format="%(asctime)s - %(levelname)s - [%(name)s] %(message)s",
             level=logging.INFO,
             handlers=[
-                logging.FileHandler("../logs/bot_debug.log"),
+                logging.FileHandler(os.path.join(log_dir, "bot_debug.log")),
                 logging.StreamHandler(),
             ]
         )
